1582cce780018ffec2253c2e448c146e0a1fc2f8
rdincher <rdincher@users.noreply.github.com>
2024-04-06 11:45:19 -0500

    feat: add c++ project

---
+++ CPP/src/HelloSample.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "HelloWorld/HelloWorld.h"
+
+int main() {
+    std::cout << get_hello_message() << std::ends;
+    return 0;
+}
---
+++ CPP/src/HelloWorld/HelloWorld.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+
+#include <string>
+
+std::string get_hello_message();
+
+#endif
